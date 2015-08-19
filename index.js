7594f8c719cba0083bb0da7676cff8a806e131fb
stevenlim89 <stevenlim890@gmail.com>
2015-08-18 23:23:39 -0700

    routes set up

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var routes = require('./routes/routes');
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -12,6 +14,9 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+app.get('/projects', routes.projects);
+app.get('/blog', routes.blog);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+exports.projects = function(req, res){
+	res.render('pages/projects');	
+};
+
+exports.blog = function(req, res){
+	res.render('pages/blog');
+}
